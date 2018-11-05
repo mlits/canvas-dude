d5baf18c2f004cb76712c9b57356b859e2712791
Bilous <d818651@gmail.com>
2018-11-06 01:22:29 +0200

    return to "prompt"

--- solution-01.js
+++ solution-01.js
@@ -8,16 +8,6 @@ drawRightHand();
 drawLeftLeg();
 drawRightLeg();
 switch (debug) {
-<<<<<<< HEAD
-  case true:
-
-    document.querySelector('h1').innerHTML = 'Draw people with debug';
-    break;
-  case false:
-    document.querySelector('h1').innerHTML = 'Draw people with out debug';
-    break;
-
-=======
   case "true":
     {
       document.querySelector('h1').innerHTML = 'Draw people with debug';
@@ -28,7 +18,6 @@ switch (debug) {
       document.querySelector('h1').innerHTML = 'Draw people with out debug';
       break;
     }
->>>>>>> parent of 28b7132... change prompt to confirm
 }
 if (debug == "true" && Date.now() - timer < 200) alert("Please on debug mode in your browser or make debug slower");
 // make legal change and commit via git bash
