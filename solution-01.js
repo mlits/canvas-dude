8f0b6e6655777ec4cb84d79c529ab1d87f12e581
Bilous <d818651@gmail.com>
2018-11-05 22:36:11 +0200

    add promt for debug

--- solution-01.js
+++ solution-01.js
@@ -1,12 +1,8 @@
+if (prompt("Debug draw process?", "false") == "true") debugger;
 drawHead();
 drawBody();
 drawLeftHand();
-var i=10;
-debugger;
-i++;
 drawRightHand();
-i++;
 drawLeftLeg();
-
 drawRightLeg();
 // make legal change and commit via git bash
