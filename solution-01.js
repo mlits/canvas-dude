9920d4ec64004c60f76f900969dbe93e71332207
Bilous <d818651@gmail.com>
2018-11-05 23:16:45 +0200

    add alert if debug mode in browser is turned off

--- solution-01.js
+++ solution-01.js
@@ -1,8 +1,11 @@
-if (prompt("Debug draw process?", "false") == "true") debugger;
+var debug=prompt("Debug draw process?", "false");
+var timer=Date.now();
+if ((debug) == "true") debugger;
 drawHead();
 drawBody();
 drawLeftHand();
 drawRightHand();
 drawLeftLeg();
 drawRightLeg();
+if (debug=="true" && Date.now()-timer<200) alert("Please on debug mode in your browser or make debug slower");
 // make legal change and commit via git bash
