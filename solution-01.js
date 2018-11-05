d5387bb2b028667eec4d44e678d3a1e5f7c28267
Bilous <d818651@gmail.com>
2018-11-06 01:21:03 +0200

    Revert "change prompt to confirm"
    This reverts commit 28b7132b75c061f304ccb68031e1f4feeae7c3e3.

--- solution-01.js
+++ solution-01.js
@@ -1,6 +1,6 @@
-var debug = confirm("Debug draw process?");
+var debug = prompt("Debug draw process? (true or false)", "false");
 var timer = Date.now();
-if ((debug) == true) debugger;
+if ((debug) == "true") debugger;
 drawHead();
 drawBody();
 drawLeftHand();
@@ -8,6 +8,7 @@ drawRightHand();
 drawLeftLeg();
 drawRightLeg();
 switch (debug) {
+<<<<<<< HEAD
   case true:
 
     document.querySelector('h1').innerHTML = 'Draw people with debug';
@@ -16,6 +17,18 @@ switch (debug) {
     document.querySelector('h1').innerHTML = 'Draw people with out debug';
     break;
 
+=======
+  case "true":
+    {
+      document.querySelector('h1').innerHTML = 'Draw people with debug';
+      break;
+    }
+  case "false":
+    {
+      document.querySelector('h1').innerHTML = 'Draw people with out debug';
+      break;
+    }
+>>>>>>> parent of 28b7132... change prompt to confirm
 }
-if (debug == true && Date.now() - timer < 200) alert("Please on debug mode in your browser or make debug slower");
+if (debug == "true" && Date.now() - timer < 200) alert("Please on debug mode in your browser or make debug slower");
 // make legal change and commit via git bash
