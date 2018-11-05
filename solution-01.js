28b7132b75c061f304ccb68031e1f4feeae7c3e3
Bilous <d818651@gmail.com>
2018-11-05 23:59:26 +0200

    change prompt to confirm

--- solution-01.js
+++ solution-01.js
@@ -1,6 +1,6 @@
-var debug = prompt("Debug draw process? (true or false)", "false");
+var debug = confirm("Debug draw process?");
 var timer = Date.now();
-if ((debug) == "true") debugger;
+if ((debug) == true) debugger;
 drawHead();
 drawBody();
 drawLeftHand();
@@ -8,16 +8,16 @@ drawRightHand();
 drawLeftLeg();
 drawRightLeg();
 switch (debug) {
-  case "true":
+  case true:
     {
       document.querySelector('h1').innerHTML = 'Draw people with debug';
       break;
     }
-  case "false":
+  case false:
     {
       document.querySelector('h1').innerHTML = 'Draw people with out debug';
       break;
     }
 }
-if (debug == "true" && Date.now() - timer < 200) alert("Please on debug mode in your browser or make debug slower");
+if (debug == true && Date.now() - timer < 200) alert("Please on debug mode in your browser or make debug slower");
 // make legal change and commit via git bash
